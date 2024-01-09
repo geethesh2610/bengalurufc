9709a5811950ecbffd6256e4f7d342c4f2e18c06
Geethesh Nair <geetheshnair123@gmail.com>
2024-01-09 10:40:08 +0530

    bengaluru fc

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,11 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ["./*.{html,js}"],
-  theme: {
-    extend: {},
-    fontFamily: {
-      space: ["Space Mono", "monospace"]
-    }
-  },
-  plugins: [],
-}
-
+	content: ["./*.{html,js}"],
+	theme: {
+		extend: {},
+		fontFamily: {
+			inter: ["Inter, sans-serif"],
+		},
+	},
+	plugins: [],
+};
